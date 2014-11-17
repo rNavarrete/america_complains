d28221c8747a1590746ac67677874e1354307a83
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-11-17 14:27:48 -0700

    added gems

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+  attr_accessor :password
+  before_save :encrypt_password
+
+  def encrypt_password
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+  end
+
+  def self.authenticate(email, password)
+    user = User.where(email: email).first
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      session[:user_id] = user.id
+    else
+      nil
+    end
+  end
 end
