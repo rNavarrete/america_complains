766b6fef48f6b0453aa363abfb02fb14844d2452
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-11-22 17:33:38 -0700

    added a new layout

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -4,11 +4,6 @@ class ComplaintsController < ApplicationController
   end
 
   def search
-  parameters = { term: params[:query], limit: 16 }
-    render json: Yelp.client.search('Denver', parameters)
-
-
-
     @query = Complaint.search do
       fulltext params[:query]
     end
