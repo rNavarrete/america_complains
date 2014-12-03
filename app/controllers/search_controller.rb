9322b57b14089614e534fbb558547b0cbbd8fbf6
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-12-02 20:01:01 -0700

    deleted solr stuff

--- app/controllers/search_controller.rb
+++
@@ -1,9 +0,0 @@
-class SearchController < ApplicationController
-
-  def search_for
-    @query = Business.search do
-      fulltext params[:query]
-    end
-    @search_results = @query.results
-  end
-end
